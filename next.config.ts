65815382eee710fa1fbd154676b76df82ea3a99d
Zenex <zenexmrkt@gmail.com>
2026-02-01 09:00:40 +0000

    اكمل تحويل التطبيق إلى تطبيق ويب تقدمي

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import withPWAInit from '@ducanh2912/next-pwa';
 
 const withPWA = withPWAInit({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
   register: true,
   scope: '/',
   sw: 'sw.js',
