34fb1bcd9a1c3dae3a3b8800d3be161bb64abf9b
Zenex <zenexmrkt@gmail.com>
2026-02-02 19:36:58 +0000

    اكمل صفحة الاعدادات

--- next.config.js
+++ next.config.js
@@ -24,6 +24,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'picsum.photos',
       },
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
     ],
   },
 };
